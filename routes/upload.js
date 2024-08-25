83220a92157a1dbaf6258e290497424d83be8240
desaimayank1 <desaimayank478@gmail.com>
2024-08-25 20:36:43 +0530

    env

--- routes/upload.js
+++ routes/upload.js
@@ -8,7 +8,7 @@ const { v4: uuid } = require("uuid");
 const bucket = new S3Client({
   region: "ap-south-1",
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   },
 });
