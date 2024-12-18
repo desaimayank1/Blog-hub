9836dedcc8f425b6e4b1bbd7ca47a3227f19c5ce
desaimayank1 <desaimayank478@gmail.com>
2024-12-18 20:49:00 +0530

    react-frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header"
+import Login from "./components/Login";
+// import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
+function App() {
+  return (
+    <>
+      <Login/>
+    </>
+  )
+}
+
+export default App
