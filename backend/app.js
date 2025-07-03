8b6cb6edceb853f2419106cdc43a35e045471673
desaimayank1 <desaimayank478@gmail.com>
2025-07-03 13:35:24 +0530

    test

--- backend/app.js
+++ backend/app.js
@@ -37,8 +37,8 @@ app.use(
     store: new MongoStore({ mongooseConnection: mongoose.connection }),
     cookie: {
       httpOnly:true,
-      secure: process.env.NODE_ENV === 'production', 
-      sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',   
+      secure: true, 
+      sameSite:'None',   
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     },
   })
